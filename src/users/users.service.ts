4c33de187290e3a333dc845f4f5ed80826f0d470
sergio.pena <sergio.pena.orozco@gmail.com>
2024-11-03 23:19:21 -0600

    add create user

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './user.entity';
+import { Repository } from 'typeorm';
+import { CreateUserDto } from './dto/create-user.dto';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+
+    constructor(@InjectRepository(User) private usersRepository: Repository<User>) {}
+
+    create(user: CreateUserDto) {
+        const newUser = this.usersRepository.create(user);
+        return this.usersRepository.save(newUser);
+    }
+}
